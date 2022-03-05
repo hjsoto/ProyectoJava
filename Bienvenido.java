ca273e735e713c625d0588a1d178a7b6f582a93d
hjsoto <hectorjaviers@gmail.com>
2022-03-05 17:29:50 -0400

    imprimiendo un ciclo simple

--- Bienvenido.java
+++ Bienvenido.java
@@ -4,5 +4,8 @@ public class Bienvenido
     {
         System.out.println("Bienvenido a la programacion en java");
         System.out.println("Agregando un parrafo en java");
+
+        for(int i = 0; i <= 10; i++)
+            System.out.println("Indice " + i);
     }
 }
