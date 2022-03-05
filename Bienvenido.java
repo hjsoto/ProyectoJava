890eccbfec62f6dda5dfc68bb09476f9f6d92c06
hjsoto <hectorjaviers@gmail.com>
2022-03-05 17:22:57 -0400

    Agregando segundo parrafo

--- Bienvenido.java
+++ Bienvenido.java
@@ -3,5 +3,6 @@ public class Bienvenido
     public static void main(String args[])
     {
         System.out.println("Bienvenido a la programacion en java");
+        System.out.println("Agregando un parrafo en java");
     }
 }
